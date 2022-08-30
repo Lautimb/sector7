a63e9de16056fe5ebe38fd36554d7d0f121f088b
Lautaro Barandiaran <lbarandiaran@lanacion.com.ar>
2022-08-30 20:13:35 -0300

    - Desafio numero 6, promises,asincronia y maps - Se crea ItemList para iterar las cards de productos - Se crea Card para mostrar los productos con las props requeridas - Se quita el saludo de los primeros desafios - Se quita ItemCount y funcion onAdd hasta aplicarlo donde se debe

--- src/components/NavBar/MenuButton.jsx
+++ src/components/NavBar/MenuButton.jsx
@@ -1,32 +1,32 @@
 import React, { useState } from "react";
-import ReactDOM from "react-dom";
+import { createPortal } from "react-dom";
 import Button from "../Common/Button";
 import Modal from "../Common/Modal";
 import Menu from "./Menu";
 const MenuButton = ({ label = "" }) => {
-  const [isOpened, setIsOpened] = useState(false);
-  const root = document.querySelector("#root");
-  const handleMenu = () => {
-    setIsOpened(!isOpened);
-  };
+    const [isOpened, setIsOpened] = useState(false);
+    const root = document.querySelector("#root");
+    const handleMenu = () => {
+        setIsOpened(!isOpened);
+    };
 
-  return (
-    <>
-      <Button onClick={handleMenu} label={label}>
-        <i className="fa-solid fa-bars" />
-      </Button>
-      {isOpened
-        ? ReactDOM.createPortal(
-            <>
-              <Modal>
-                <Menu handleClose={setIsOpened} />
-              </Modal>
-            </>,
-            root
-          )
-        : null}
-    </>
-  );
+    return (
+        <>
+            <Button onClick={handleMenu} label={label}>
+                <i className="fa-solid fa-bars" />
+            </Button>
+            {isOpened
+                ? createPortal(
+                      <>
+                          <Modal>
+                              <Menu handleClose={setIsOpened} />
+                          </Modal>
+                      </>,
+                      root
+                  )
+                : null}
+        </>
+    );
 };
 
 export default MenuButton;
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -3,26 +3,26 @@ import "./navbar.scss";
 import MenuButton from "./MenuButton";
 import SecondListIcons from "./SecondListIcons";
 const NavBar = () => {
-  return (
-    <nav className="navbar-mobile">
-      <div className="wrapper-site">
-        <ul>
-          <li>
-            <a href="/home">
-              <i className="fa-solid fa-house" />
-              home
-            </a>
-          </li>
-          <li>
-            <MenuButton label="menu">
-              <i className="fa-solid fa-bars" />
-            </MenuButton>
-          </li>
-          <SecondListIcons />
-        </ul>
-      </div>
-    </nav>
-  );
+    return (
+        <nav className="navbar-mobile">
+            <div className="wrapper-site">
+                <ul>
+                    <li>
+                        <a href="/home">
+                            <i className="fa-solid fa-house" />
+                            home
+                        </a>
+                    </li>
+                    <li>
+                        <MenuButton label="menu">
+                            <i className="fa-solid fa-bars" />
+                        </MenuButton>
+                    </li>
+                    <SecondListIcons />
+                </ul>
+            </div>
+        </nav>
+    );
 };
 
 export default NavBar;
